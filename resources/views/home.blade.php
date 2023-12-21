ec50173645f4c7b064e392cdff92bd4bc3c70788
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-12-21 16:25:12 +0100

    added table with trains informations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,42 @@
 @section('title', 'Home')
 
 @section('content')
-<main>
-    <h1>Home page</h1>
-</main>
+    <main>
+        <div class="container mt-5">
+            <table class="table p-5">
+                <thead>
+                    <tr>
+                        <th scope="col">Azienda</th>
+                        <th scope="col">Partenza</th>
+                        <th scope="col">Arrivo</th>
+                        <th scope="col">Binario</th>
+                        <th scope="col">Orario Partenza</th>
+                        <th scope="col">Orario Arrivo</th>
+                        <th scope="col">Codice Treno</th>
+                        <th scope="col">N. Carrozze</th>
+                        <th scope="col">Ritardo</th>
+                        <th scope="col">Cancellati</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($trains as $train)
+                        <tr>
+                            <td>{{ $train->company }}</td>
+                            <td>{{ $train->departure_station }}</td>
+                            <td>{{ $train->arrival_station }}</td>
+                            <td>{{ $train->train_track }}</td>
+                            <td>{{ $train->departure_time }}</td>
+                            <td>{{ $train->arrival_time }}</td>
+                            <td>{{ $train->train_code }}</td>
+                            <td>{{ $train->carriages_count }}</td>
+                            <td>{{ $train->on_time ? 'In orario' : 'In ritardo' }}</td>
+                            <td>{{ $train->canceled ? ' ' : 'Cancellato' }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        </div>
+    </main>
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,5 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<section class="bg-black">
+    <div class="container">
+        <h1 class="text-center p-2">Stazione dei treni</h1>
+    </div>
+</section>
